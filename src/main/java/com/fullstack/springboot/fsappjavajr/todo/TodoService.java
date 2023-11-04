888be48e073d6fcf1b566a8e0669abf64cfd837d
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-04 15:25:40 -0300

    Creamos funciones para manejo de CRUD de TODOS
    Incluye métodos cómo:
    -. búsqueda de TODOs por usuario
    -. Agregar nuevo TODO
    -. Eliminar TODO por id de usuario
    -. Búsqueda de TODO por id
    -. Actualizar un TODO

--- src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoService.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoService.java
@@ -3,20 +3,47 @@ package com.fullstack.springboot.fsappjavajr.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 	private static List<Todo> todos = new ArrayList<>();
-	
+
 	private static int todosCount = 0;
-	
+
 	static {
-		todos.add(new Todo(++todosCount, "in28minutes","Get AWS Certified", 
-							LocalDate.now().plusYears(10), false ));
-		todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(11), false ));
-		todos.add(new Todo(++todosCount, "in28minutes","Learn Full Stack Development", 
-				LocalDate.now().plusYears(12), false ));
+		todos.add(new Todo(++todosCount, "in28minutes", "Get AWS Certified", LocalDate.now().plusYears(10), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "Learn DevOps", LocalDate.now().plusYears(11), false));
+		todos.add(new Todo(++todosCount, "in28minutes", "Learn Full Stack Development", LocalDate.now().plusYears(12),
+				false));
+	}
+
+	public List<Todo> findTodosByUser(String user) {
+		Predicate<? super Todo> predicate = todo -> todo.getUser().equalsIgnoreCase(user);
+		return todos.stream().filter(predicate).toList();
+	}
+
+	public Todo addTodo(String user, String description, LocalDate date, boolean done) {
+		Todo todo = new Todo(++todosCount, user, description, date, done);
+		todos.add(todo);
+		return todo;
+	}
+
+	public void deleteTodoByUserId(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
+
+	public Todo findTodoById(int id) {
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		Todo todo = todos.stream().filter(predicate).findFirst().get();
+		return todo;
+	}
+
+	public void updateTodo(Todo todo) {
+		deleteTodoByUserId(todo.getId());
+		todos.add(todo);
 	}
 }
