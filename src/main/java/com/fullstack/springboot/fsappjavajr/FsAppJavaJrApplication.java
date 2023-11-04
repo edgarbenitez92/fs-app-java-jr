12a214078f7f25dc1ec34dcd81b79b865d9dc644
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-04 14:29:06 -0300

    Creación de proyecto Spring Boot Java
    -. Versión Java 17
    -. Maven
    -. Dependencias starter web y devtools

---
+++ src/main/java/com/fullstack/springboot/fsappjavajr/FsAppJavaJrApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack.springboot.fsappjavajr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsAppJavaJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FsAppJavaJrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fullstack/springboot/fsappjavajr/FsAppJavaJrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.springboot.fsappjavajr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsAppJavaJrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
