e1fb30f9fbf08da96fe988d74a48dcae33f23fcb
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-06 17:52:44 -0300

    Eliminación de annotation innecesaria debido a la corrección de la raíz

--- src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
@@ -2,13 +2,13 @@ package com.fullstack.springboot.fsappjavajr.todo;
 
 import java.util.List;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class TodoResource {
 
 	private TodoService todoService;
