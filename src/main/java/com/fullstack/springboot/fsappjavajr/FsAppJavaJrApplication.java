cd8e9b60b5a1542cd74a2f2d62ae13e9188fee5b
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-06 13:01:45 -0300

    Configuración para evitar block por CORS
    -. Se intentó realizarlo desde la raíz del proyecto pero no funcionó
    -. Se optó por usar la annotation CrossOrigen dentro del TodoResource y funcionó correctamente el consumo del servicio desde el front

--- src/main/java/com/fullstack/springboot/fsappjavajr/FsAppJavaJrApplication.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/FsAppJavaJrApplication.java
@@ -2,6 +2,9 @@ package com.fullstack.springboot.fsappjavajr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FsAppJavaJrApplication {
@@ -9,5 +12,16 @@ public class FsAppJavaJrApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FsAppJavaJrApplication.class, args);
 	}
+
+	// @Bean
+	// public WebMvcConfigurer corsConfigurer() {
+	// return new WebMvcConfigurer() {
+	// public void addCorsMapping(CorsRegistry registry) {
+	// registry.addMapping("/**")
+	// .allowedMethods("*")
+	// .allowedOrigins("http://localhost:5050");
+	// }
+	// };
+	// }
 
 }
--- src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
@@ -2,11 +2,13 @@ package com.fullstack.springboot.fsappjavajr.todo;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TodoResource {
 
 	private TodoService todoService;
