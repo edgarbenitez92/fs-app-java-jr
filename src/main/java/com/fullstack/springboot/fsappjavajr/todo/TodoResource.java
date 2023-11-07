5bb72253488080738692896f0b705f0984a55c41
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-07 11:28:32 -0300

    Creamos endpoint para actualizar un TODO existente

--- src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
@@ -6,6 +6,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -31,5 +33,11 @@ public class TodoResource {
 	public ResponseEntity<Void> deleteTodoById(@PathVariable String user, @PathVariable int id) {
 		todoService.deleteTodoByUserId(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("users/{user}/todos/{id}")
+	public Todo updateTodo(@PathVariable String user, @PathVariable int id, @RequestBody Todo todo) {
+		todoService.updateTodo(todo);
+		return todo;
 	}
 }
