d1c60d92a4cb4efe737bd2e1b83fc1852d75cd14
Edgar Benitez <edgarbenitez92@gmail.com>
2023-11-07 13:11:08 -0300

    Creamos POST para creación de TODO

--- src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
+++ src/main/java/com/fullstack/springboot/fsappjavajr/todo/TodoResource.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,9 +36,14 @@ public class TodoResource {
 		return ResponseEntity.noContent().build();
 	}
 
-	@PutMapping("users/{user}/todo-update")
+	@PutMapping("users/{user}/update-todo")
 	public Todo updateTodo(@PathVariable String user, @RequestBody Todo todo) {
 		todoService.updateTodo(todo);
 		return todo;
+	}
+
+	@PostMapping("users/{user}/create-todo")
+	public Todo createTodo(@PathVariable String user, @RequestBody Todo todo) {
+		return todoService.addTodo(todo.getUser(), todo.getDescription(), todo.getTargetDate(), false);
 	}
 }
